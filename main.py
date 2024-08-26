c95927d3d8ff790f45108a5c4f1829c674ad8de9
Ant1955 <abb1955@gmail.com>
2024-08-26 10:46:28 +0300

    created task tj02 draft

--- main.py
+++ main.py
@@ -2,22 +2,104 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, FSInputFile
-from config import TOKEN
+from config import TOKEN, WEATHER_API_KEY
 import random
 from datetime import datetime
 import requests
 from gtts import gTTS
 import os
 from googletrans import Translator
-
+import sqlite3
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
+import aiohttp
+import logging
 
   #  @Advt2024r_bot.
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+conn = sqlite3.connect('bot.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    username TEXT,
+    chat_id INTEGER)''')
+
+conn.commit()
+conn.close()
+
+logging.basicConfig(level=logging.INFO)
+
+class Form(StatesGroup):
+    name = State()
+    age = State()
+    city = State()
+
+def init_db():
+    conn = sqlite3.connect('user_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+	CREATE TABLE IF NOT EXISTS users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	age INTEGER NOT NULL,
+	city TEXT NOT NULL)
+	''')
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@dp.message(Form.name)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Сколько тебе лет?")
+    await state.set_state(Form.age)
+
+@dp.message(Form.age)
+async def age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("Из какого ты города?")
+    await state.set_state(Form.city)
+
+@dp.message(Form.city)
+async def city(message: Message, state:FSMContext):
+    await state.update_data(city=message.text)
+
+    user_data = await state.get_data()
+
+    conn = sqlite3.connect('user_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+    INSERT INTO users (name, age, city) VALUES (?, ?, ?)''', (user_data['name'], user_data['age'], user_data['city']))
+    conn.commit()
+    conn.close()
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                f"http://api.openweathermap.org/data/2.5/weather?q={user_data['city']}&appid={WEATHER_API_KEY}&units=metric") as response:
+            if response.status == 200:
+                weather_data = await response.json()
+                main = weather_data['main']
+                weather = weather_data['weather'][0]
+                temperature = main['temp']
+                humidity = main['humidity']
+                description = weather['description']
+                weather_report = (f"Город - {user_data['city']}\\n"
+                              f"Температура - {temperature}\\n"
+                              f"Влажность воздуха - {humidity}\\n"
+                              f"Описание погоды - {description}")
+                await message.answer(weather_report)
+            else:
+                await message.answer("Не удалось получить данные о погоде")
+    await state.clear()
+
 
 def get_weather(city):
-    api_key = "fda902a077e24b262b0187ca6bc24204"
+    api_key = WEATHER_API_KEY
    #адрес, по которомы мы будем отправлять запрос. Не забываем указывать f строку.
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
    #для получения результата нам понадобится модуль requests
@@ -27,8 +109,9 @@ def get_weather(city):
 
 
 @dp.message(CommandStart())
-async def start(message: Message):
-    await message.answer("Приветики, я бот!")
+async def start(message: Message, state: FSMContext):
+    await message.answer("Привет! Как тебя зовут?")
+    await state.set_state(Form.name)
 
 @dp.message(Command('help'))
 async def help(message: Message):
@@ -102,6 +185,7 @@ async def start(message: Message):
     except Exception as e:
         print(f"ошибка {e}")
         await message.answer("Произошла ошибка при переводе.")
+
 
 
 async def main():
